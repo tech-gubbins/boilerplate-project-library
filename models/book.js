99ba33eded38869f27a73881d85bb819af44b4c7
Joshua McNeel <joshmcneel@gmail.com>
2024-12-11 15:03:44 -0500

    feat: created BookSchema

---
+++ models/book.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	comments: [
+		{
+			type: String,
+		},
+	],
+	commentCount: {
+		type: Number,
+		default: 0,
+	},
+});
+
+module.exports = mongoose.model('Book', BookSchema);
